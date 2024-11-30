050515e1754a191b88eb33bbf3172923013f84a2
snow <snowsuika@gmail.com>
2024-11-30 14:30:46 +0800

    feat: 安裝 BootStrap、客製化樣式
    - Add Bootstrap v5.x
    - 修正 sass 版本問題

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ["@/assets/stylesheets/all.scss"],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@/assets/stylesheets/_variables.scss";'
-        }
-      }
-    }
-  }
 });
