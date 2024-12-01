7fed54906f63d59be9baba68f934712a3debd504
snow <snowsuika@gmail.com>
2024-12-01 20:44:02 +0800

    feat: 新增 404 頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,19 @@
+<script setup>
+definePageMeta({
+  layout: false
+});
+
+const router = useRouter();
+const route = useRoute();
+
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+
+setTimeout(() => {
+  router.push('/');
+}, 2000);
+</script>
+
+<template>
+  <h1>404 - {{ route.fullPath }} 找不到頁面</h1>
+</template>
